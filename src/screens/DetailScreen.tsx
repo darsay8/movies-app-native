bbde7065b7f30fb3f889f3c84bec11972c9a4a9f
darsay8 <darsay.one@gmail.com>
2023-02-20 14:50:33 -0300

    🚀

---
+++ src/screens/DetailScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+const DetailScreen = () => {
+  return (
+    <View>
+      <Text>Detail</Text>
+    </View>
+  );
+};
+export default DetailScreen;
---
+++ src/screens/HomeScreen.tsx
@@ -0,0 +1,10 @@
+import {Text, View} from 'react-native';
+
+const HomeScreen = () => {
+  return (
+    <View>
+      <Text>🍿</Text>
+    </View>
+  );
+};
+export default HomeScreen;
