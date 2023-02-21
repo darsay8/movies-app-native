4c1c4912c7e46835abe1a644058da48c96ce97af
darsay8 <darsay.one@gmail.com>
2023-02-20 22:11:06 -0300

    carousel

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,8 +1,17 @@
 import {useEffect} from 'react';
-import {ActivityIndicator, Text, View} from 'react-native';
+import {
+  ActivityIndicator,
+  Dimensions,
+  Text,
+  View,
+  FlatList,
+} from 'react-native';
 import {useSafeAreaInsets} from 'react-native-safe-area-context';
+import Carousel from 'react-native-snap-carousel';
 import MoviePoster from '../components/MoviePoster';
 import useMovies from '../hooks/useMovies';
+
+const {width: windowWidth} = Dimensions.get('window');
 
 const HomeScreen = () => {
   const {nowPlayingMovies, isLoading} = useMovies();
@@ -20,7 +29,14 @@ const HomeScreen = () => {
 
   return (
     <View style={{marginTop: top + 20}}>
-      <MoviePoster movie={nowPlayingMovies[0]} />
+      <View style={{height: 440}}>
+        <Carousel
+          data={nowPlayingMovies}
+          renderItem={({item}: any) => <MoviePoster movie={item} />}
+          sliderWidth={windowWidth}
+          itemWidth={300}
+        />
+      </View>
     </View>
   );
 };
